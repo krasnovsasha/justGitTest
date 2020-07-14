930cd70a1572a66969a83c5c034a0fe967400363
Alexander <pancasancha4@gmail.com>
2020-07-14 16:54:40 +0300

    добавлены восклицательные знаки в приветствии

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("HelloAll");
-		System.out.println("Hello");
+		System.out.println("HelloWorld!!!");
+		System.out.println("HelloAll!!!");
+		System.out.println("Hello!!!");
 	}
 }
