a04f6a34aa65af5a01ed17564d4bf9437739919c
Alexander <pancasancha4@gmail.com>
2020-07-14 21:53:03 +0300

    add 2 new files from cmd.exe

---
+++ Freedom.java
@@ -0,0 +1,5 @@
+class Freedom{
+	public static void main(String[] args){
+		System.out.println("It's freedom");
+	}
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld {
 		System.out.println("HelloAll!!!");
 		System.out.println("Hello!!!");
 		System.out.println("Greetings my dear friend");
+		System.out.println("New file Freedom.java");
 	}
 }
